b63d74ce2c963a2bcc975076b162c3e1eb394e39
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2023-11-14 18:10:27 +0700

    membuat komponen Row untuk membuat button cancel dan submit

--- app/src/main/java/com/example/esjumbo/HalamanForm.kt
+++ app/src/main/java/com/example/esjumbo/HalamanForm.kt
@@ -2,11 +2,13 @@ package com.example.esjumbo
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
@@ -18,6 +20,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
@@ -78,5 +81,15 @@ fun HalamanForm() {
             label = { Text(text = "Nomor Telepon") })
 
         Spacer(modifier = Modifier.padding(16.dp))
+
+        Row {
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = stringResource(id = R.string.cancel))
+            }
+
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = stringResource(id = R.string.submit))
+            }
+        }
     }
 }
