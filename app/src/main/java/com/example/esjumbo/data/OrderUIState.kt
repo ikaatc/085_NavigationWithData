17c7cbf0b78157d3aab2c1318719505f7ead5588
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2023-11-14 17:09:05 +0700

    menambahkan data class UIState untuk menyimpan dan mengelola informasi

--- app/src/main/java/com/example/esjumbo/data/OrderUIState.kt
+++ app/src/main/java/com/example/esjumbo/data/OrderUIState.kt
@@ -3,5 +3,8 @@ package com.example.esjumbo.data
 data class OrderUIState(
     val jumlah: Int = 0,
     val rasa: String = "",
-    val harga: String = ""
+    val harga: String = "",
+    val nama: String = "",
+    val alamat: String = "",
+    val noTelp: String = ""
 )
