0fffd20c06cc7f81c0eff72a7edec94cb48357ce
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2023-11-14 17:48:20 +0700

    membuat komponen OutlinedTeksField untuk input nomor telepon

--- app/src/main/java/com/example/esjumbo/HalamanForm.kt
+++ app/src/main/java/com/example/esjumbo/HalamanForm.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
@@ -17,6 +18,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -58,5 +60,14 @@ fun HalamanForm() {
             modifier = Modifier.fillMaxWidth(),
             onValueChange = { textAlamat = it },
             label = { Text(text = "Alamat") })
+
+        OutlinedTextField(
+            value = textTlp,
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            shape = MaterialTheme.shapes.large,
+            modifier = Modifier.fillMaxWidth(),
+            onValueChange = { textTlp = it },
+            label = { Text(text = "Nomor Telepon") })
     }
 }
