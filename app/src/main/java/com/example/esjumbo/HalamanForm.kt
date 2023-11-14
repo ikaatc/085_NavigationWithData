8a2e1b8cc533e42e4a8b4917ab106bf950a17ef2
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2023-11-14 17:38:27 +0700

    membuat komponen Column yang menampilkan form data pelanggan dan membuat teks judul data pelanggan nya

--- app/src/main/java/com/example/esjumbo/HalamanForm.kt
+++ app/src/main/java/com/example/esjumbo/HalamanForm.kt
@@ -1,11 +1,19 @@
 package com.example.esjumbo
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Preview
 @Composable
@@ -15,4 +23,14 @@ fun HalamanForm() {
     var textTlp by remember { mutableStateOf("") }
 
     var ListDataTxt: MutableList<String> = mutableListOf(textNama, textAlamat, textTlp)
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxSize()
+    ) {
+        Text(text = "Data Pelanggan")
+    }
 }
