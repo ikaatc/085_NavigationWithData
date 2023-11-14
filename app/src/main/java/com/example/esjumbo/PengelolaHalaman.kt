e3b0fa36811c83216571474cd096ad4cbca74b1f
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2023-11-14 18:44:52 +0700

    menambahkan fungsi untuk membatalkan pesanan dan navigasi ke halaman Home

--- app/src/main/java/com/example/esjumbo/PengelolaHalaman.kt
+++ app/src/main/java/com/example/esjumbo/PengelolaHalaman.kt
@@ -86,7 +86,7 @@ fun EsJumboApp(
                     onSubmitButtonClick = {
                         viewModel.setContact(it)
                         navController.navigate(PengelolaHalaman.Rasa.name)},
-                    onCancelButtonClick = { }
+                    onCancelButtonClick = { cancelOrderAndNavigateToHome(navController) }
                 )
             }
 
@@ -99,7 +99,7 @@ fun EsJumboApp(
                     onSelectionChanged = { viewModel.setRasa(it) },
                     onConfirmButtonClicked = { viewModel.setJumlah(it) },
                     onNextButtonClicked = { navController.navigate(PengelolaHalaman.Summary.name) },
-                    onCancelButtonClicked = { cancelOrderAndNavigateToHome(viewModel, navController) }
+                    onCancelButtonClicked = { cancelOrderAndNavigateToForm(viewModel, navController) }
                 )
             }
 
@@ -113,7 +113,7 @@ fun EsJumboApp(
     }
 }
 
-private fun cancelOrderAndNavigateToHome(
+private fun cancelOrderAndNavigateToForm(
     viewModel: OrderViewModel,
     navController: NavHostController
 ) {
@@ -125,4 +125,10 @@ private fun cancelOrderAndNavigateToRasa(
     navController: NavHostController
 ) {
     navController.popBackStack(PengelolaHalaman.Rasa.name, inclusive = false)
+}
+
+private fun cancelOrderAndNavigateToHome(
+    navController: NavHostController
+) {
+    navController.popBackStack(PengelolaHalaman.Home.name, inclusive = false)
 }
